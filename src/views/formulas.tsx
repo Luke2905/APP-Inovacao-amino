e038ae12d64a7066e55c04b82b3b4e24cf96bc05
Lucas Laranjeira <88212940+Luke2905@users.noreply.github.com>
2025-05-09 16:57:30 -0300

    Update formulas: Inclusão da tabela com os componentes da formula no detalhamento

--- src/views/formulas.tsx
+++ src/views/formulas.tsx
@@ -24,7 +24,11 @@ function List() {
   const [loading, setLoading] = useState(true);
   const [mostrarModal, setMostrarModal] = useState(false);
   const [formulaSelecionada, setFormulaSelecionada] = useState(null);
+  const [mostrarModalEdit, setMostrarModalEdit] = useState(false);
+  const [formulaSelecionadaEdit, setFormulaSelecionadaEdit] = useState(null);
 
+
+  /*---------------------- Função Listar as Formulações ----------------- */
   async function getFormulas() {
     try {
       const formulaApi = await api.get('/formulas');
@@ -35,6 +39,11 @@ function List() {
     }
   }
 
+  useEffect(() => {
+    getFormulas();
+  }, []);
+
+  /*-------------------------- Função para Deletar uma Formulação --------------- */
   async function deleteFormulas(id) {
     try {
       const response = await api.delete(`/deletarFormula/${id}`);
@@ -46,8 +55,9 @@ function List() {
     }
   }
 
+  /*----------------- Função Para Exibir os Detalhes de uma Formulação ---------------------*/
   async function visualizarFormula(id) {
-    console.log("Buscando fórmula com ID:", id); // <-- ADICIONE ISSO
+    console.log("Buscando fórmula com ID:", id);
     try {
       const response = await api.get(`/formula/${id}`);
       setFormulaSelecionada(response.data);
@@ -58,15 +68,66 @@ function List() {
     }
   }
 
-  useEffect(() => {
-    getFormulas();
-  }, []);
+  /* -------------------- Função para Editar uma Formulação ----------------------------- */
+  async function visualizarFormulaEdit(id) {
+    console.log("Buscando fórmula com ID:", id); 
+    try {
+      const response = await api.get(`/formula/${id}`);
+      setFormulaSelecionadaEdit(response.data);
+      setMostrarModalEdit(true);
+    } catch (error) {
+      console.error("Erro ao buscar fórmula:", error);
+      alert("Erro ao buscar fórmula.");
+    }
+  }
 
+      // Atualização/Edição de Formulações
+      const [tituloEdit, setTituloEdit] = useState('');
+      const [precoEdit, setPrecoEdit] = useState('');
+      const [codigoEdit, setCodigoEdit] = useState('');
+      const [descricaoEdit, setDescricaoEdit] = useState('');
+      const [justificativaEdit, setJustificativaEdit] = useState('');
+
+
+      useEffect(() => {
+        if (formulaSelecionadaEdit) {
+          setTituloEdit(formulaSelecionadaEdit.titulo);
+          setPrecoEdit(formulaSelecionadaEdit.preco);
+          setCodigoEdit(formulaSelecionadaEdit.codigo);
+          setDescricaoEdit(formulaSelecionadaEdit.descricao);
+          setJustificativaEdit(formulaSelecionadaEdit.justificativa);
+        }
+      }, [formulaSelecionadaEdit]);
+
+      async function salvarEdicao() {
+        try {
+          await api.put(`/alterarFormula/${formulaSelecionadaEdit.id}`, {
+            titulo: tituloEdit,
+            preco: precoEdit,
+            codigo: codigoEdit,
+            descricao: descricaoEdit,
+            justificativa: justificativaEdit
+          });
+          alert('Fórmula atualizada com sucesso!');
+          setMostrarModalEdit(false);
+          window.location.reload();
+        } catch (err) {
+          console.error('Erro ao atualizar fórmula:', err);
+          alert('Erro ao atualizar fórmula.');
+        }
+      }
+
+  /* ------------------------------------ X --------------------------------------- */
+
+
+  /* -------------------- Barra de pesquisa e Filtros ----------------------------- */
   const formulasFiltradas = formulas.filter(item =>
     (!pesquisa || item.codigo?.toLowerCase().includes(pesquisa.toLowerCase()) ||
       item.titulo?.toLowerCase().includes(pesquisa.toLowerCase()))
   );
 
+
+  /* ---------------------- Bloco Visual --------------------------------------------- */
   return (
     <div className="lista-container" style={{ padding: '20px' }}>
       <input
@@ -87,7 +148,7 @@ function List() {
               <h3>{formula.titulo}</h3>
               <p>{formula.descricao}</p>
               <i className="fa-solid fa-eye" onClick={() => visualizarFormula(formula.id)}></i>
-              <i className="fa-solid fa-pencil"></i>
+              <i className="fa-solid fa-pencil" onClick={() => visualizarFormulaEdit(formula.id)}></i>
               <i className="fa-solid fa-trash" onClick={() => deleteFormulas(formula.id)}></i>
             </div>
           ))}
@@ -95,19 +156,17 @@ function List() {
 
       {/* Modal Visualização */}
       {mostrarModal && formulaSelecionada && (
-        <div className="VerFormulas" >
+        <div className="VerFormulas">
           <motion.div
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <span
-              className="close"
-              onClick={() => setMostrarModal(false)}
-            >
+            <span className="close" onClick={() => setMostrarModal(false)}>
               x
             </span>
             <h2>Detalhes da Fórmula</h2>
+
             <label>Título</label>
             <input type="text" value={formulaSelecionada.titulo} disabled />
             <label>Preço</label>
@@ -118,6 +177,69 @@ function List() {
             <input type="text" value={formulaSelecionada.descricao} disabled />
             <label>Justificativa</label>
             <input type="text" value={formulaSelecionada.justificativa} disabled />
+
+            <hr />
+            <h3 style={{textAlign: 'center'}}>Componentes</h3>
+          {formulaSelecionada.componentes && formulaSelecionada.componentes.length > 0 ? (
+            // Se a fórmula tiver componentes, renderiza a tabela
+            <table className="Tabela-componete">
+              <thead>
+                <tr>
+                  <th>Componente</th>
+                  <th>Ordem</th>
+                  <th>Proporção</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* Percorre o array de componentes e cria uma linha (tr) para cada componente */}
+                {formulaSelecionada.componentes.map((comp, index) => (
+                  <tr key={index}>
+                    <td>{comp.componente}</td>
+                    <td>{comp.ordem}</td>
+                    <td>{comp.proporcao}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            // Caso não existam componentes, mostra uma mensagem informando
+            <p style={{ marginTop: '10px', fontStyle: 'italic', color: 'gray' }}>
+              Nenhum componente registrado para esta fórmula.
+            </p>
+          )}
+
+          </motion.div>
+        </div>
+      )}
+
+
+            {/* Modal Visualização Editavel */}
+            {mostrarModalEdit && formulaSelecionadaEdit && (
+        <div className="VerFormulas" >
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <span
+              className="close"
+              onClick={() => setMostrarModalEdit(false)}
+            >
+              x
+            </span>
+            <h2>Alteração de Fórmula</h2>
+            <label>Título</label>
+            <input type="text" value={tituloEdit} onChange={(e) => setTituloEdit(e.target.value)} />  {/*-> forma de editar os dados das formulações*/}
+            <label>Preço</label>
+            <input type="number" value={precoEdit} onChange={(e) => setPrecoEdit(e.target.value)} />
+            <label>Código</label>
+            <input type="number" value={codigoEdit} onChange={(e) => setCodigoEdit(e.target.value)} />
+            <label>Descrição</label>
+            <input type="text" value={descricaoEdit} onChange={(e) => setDescricaoEdit(e.target.value)} />
+            <label>Justificativa</label>
+            <input type="text" value={justificativaEdit} onChange={(e) => setJustificativaEdit(e.target.value)} />
+
+            <button id="salvar" onClick={salvarEdicao}>Salvar</button>
           </motion.div>
         </div>
       )}
@@ -126,3 +248,8 @@ function List() {
 }
 
   
+
+
+// formulaSelecionada.componentes && formulaSelecionada.componentes.length > 0
+//   ? (/* renderiza tabela */) //-> Se a condição for verdadeira
+//   : (/* renderiza mensagem de "nenhum componente" */) //-> se for falsa
