ba200c31871c534e3ae214d2dd1b616c04ce5adf
Lucas Laranjeira <88212940+Luke2905@users.noreply.github.com>
2025-05-12 14:59:10 -0300

    Update busca: Modal de detalhamento da fórmulas e visualização de propriedades

--- src/views/buscaAvancada.tsx
+++ src/views/buscaAvancada.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import '../App.css'
 import api from "../service/api";
 import {motion} from 'framer-motion'; //-> biblioteca para animações
+import { div } from "framer-motion/client";
 
 const BuscaPropriedades =() =>{
         return(
@@ -23,8 +24,9 @@ function List() {
     const [pesquisa, setPesquisa] = useState('');
     const [filtrosAplicacaoSelecionados, setFiltrosAplicacaoSelecionados] = useState([]);
     const [filtrosFamiliaSelecionados, setFiltrosFamiliaSelecionados] = useState([]);
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [formulaSelecionada, setFormulaSelecionada] = useState(null);
     
-
     function handleFiltroAplicacaoChange(event) { //handle para filtro de aplicações
         const { value, checked } = event.target;
         if (checked) {
@@ -85,7 +87,25 @@ const Formulas = []
       getFamilia();
     }, []);
 
-    
+  const limparFiltros = () => {
+    setPesquisa(""); // Limpa o campo de pesquisa
+    setFiltrosAplicacaoSelecionados([]);
+    setFiltrosFamiliaSelecionados([]); 
+  };
+
+    async function VisualizarProrpiedades(id) {
+      console.log("Buscando fórmula com ID:", id);
+      try {
+        const response = await api.get(`/formulapropriedades/${id}`);
+        setFormulaSelecionada(response.data);
+        console.log(response.data)
+        setMostrarModal(true);
+      } catch (error) {
+        console.error("Erro ao buscar fórmula:", error);
+        alert("Erro ao buscar fórmula.");
+      }
+      
+    }
   
     const formulasFiltradas = formulas.filter(item => {
         const pesquisaOk = !pesquisa || 
@@ -113,6 +133,7 @@ const Formulas = []
                 value={pesquisa}
                 onChange={(e) => setPesquisa(e.target.value)}
             />
+            <button className="limpaFiltro" onClick={() => limparFiltros()}><i className="fa-solid fa-filter-circle-xmark"></i> Limpar Filtros</button>
         <div className="buscaContainer">
             <div className="select-propriedades">
             <strong style={{textAlign: 'center'}}>Filtrar por Familia:</strong>
@@ -160,6 +181,7 @@ const Formulas = []
                     <th>Familia</th>
                     <th>Aplicação</th>
                     <th>Descrição</th>
+                    <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -171,12 +193,67 @@ const Formulas = []
                     <td>{formula.familia_titulo}</td>
                     <td>{formula.aplicacao_titulo}</td>
                     <td>{formula.descricao}</td>
+                    <td>   <i className="fa-solid fa-eye" onClick={() => VisualizarProrpiedades(formula.id)}></i></td>
                     </tr>
                 ))}
                 </tbody>
                 </table>
             </div>
 
+                {mostrarModal && formulaSelecionada &&(
+                  <div className="VerPropriedades" >
+                    <motion.div
+                      initial={{ opacity: 0, y: -50 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      transition={{ duration: 0.5 }}
+                    >
+                    <div className="detalhesProp">
+                     <span
+                      className="close"
+                      onClick={() => setMostrarModal(false)}
+                    >
+                      x
+                    </span>
+                    <h2>Detalhamento</h2>
+                      <label>Nome: </label><br />
+                      <input type="text" value={formulaSelecionada.nome} disabled />
+                      <hr />
+                      <label>Descricão: </label><br />
+                      <input type="text" value={formulaSelecionada.descricao} disabled />
+                      <hr />
+                      <label>Familia: </label>
+                      <input type="text" value={formulaSelecionada.familia} disabled />
+                      <label> Aplicação: </label>
+                      <input type="text" value={formulaSelecionada.aplicacao} disabled />
+                      <hr />
+                      <label >Revisão: </label>
+                      <input type="text" value={formulaSelecionada.revisao} disabled />
+                      <label >Data da Revisão: </label>
+                      <input type="text" value={formulaSelecionada.dt_revisao} disabled />
+                      <hr />
+                      <h4>Propriedades</h4>
+                    </div>
+
+
+             {formulaSelecionada.propriedades && formulaSelecionada.propriedades.length > 0 ?(
+                <>
+              {formulaSelecionada.propriedades.map((comp, index) => (
+                   
+                <div className="CardPropriedade" key={index}>  
+                    <p>{comp.propriedade}</p>
+                    <p>Valor Min: {comp.valorVariacaoInferior} ↑</p>   
+                    <p>Valor Max: {comp.valorVariacaoSuperior} ↓</p>
+             </div>
+                ))}
+                </>   
+            ) : (
+              <div>
+                sem propriedades cadastradas
+              </div>
+            )}
+                </motion.div>
+                </div>  
+                )}
             </div>
         </>
     );
