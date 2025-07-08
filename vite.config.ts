9d37f4d5e898fdc77e6c6cdf3debe940350db591
Lucas Laranjeira <88212940+Luke2905@users.noreply.github.com>
2025-07-08 16:04:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,           // ← Porta padrão
+    open: true,           // ← Abre ao executar o RUN
+    proxy: {
+      '/api': {
+        target: 'http://10.0.3.2:5678',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
+});
