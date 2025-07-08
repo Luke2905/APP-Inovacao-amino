9ab1c28d0f3be4abde2c4b3220dd7fe787acf19e
Lucas Laranjeira <88212940+Luke2905@users.noreply.github.com>
2025-07-08 16:09:41 -0300

    Criação dos itens de menu de usuario
    Menu principal divido para que só usuarios com determinados acessos possam acessar as paginas

---
+++ src/components/SIdeMenuItens.tsx/menuLaboratorio.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import '../../App.css';
+import App from '../../App';
+import { Outlet, Link } from "react-router-dom";
+
+const MenuLaboratorio = () => {
+  const [expandido, setExpandido] = useState(false); // -> estado de expansão do meno
+  const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setExpandido(!expandido);
+  };
+
+  const handleLogout = () => {
+  localStorage.removeItem('token');
+  navigate('/login');
+  };
+
+  return (
+    <>
+    <div className={`menu-lateral ${expandido ? 'expandido' : ''}`}>
+    <button className={`toggle-btn fa ${expandido ? 'fa-minus' : 'fa-bars'}`} onClick={toggleMenu}>
+    </button>
+      <h4>AMINO</h4>
+      <div className="itens-menu">
+        <ul>
+        <li>
+             <Link to="/home">
+                <i className={`fa ${expandido ? 'fa-home' : 'fa-home'}`}></i>
+                {expandido && <span>Home</span>}
+            </Link>
+         </li>
+         <li>
+             <Link to="/aplicacao">
+                <i className={`fa ${expandido ? 'fa-wand-magic-sparkles' : 'fa-wand-magic-sparkles'}`}></i>
+                {expandido && <span>Aplicações</span>}
+            </Link>
+         </li>
+         <li>
+             <Link to="/formulas">
+                <i className={`fa ${expandido ? 'fa-flask' : 'fa-flask'}`}></i>
+                {expandido && <span>Formulas</span>}
+            </Link>
+         </li>
+         <li>
+             <Link to="/busca">
+                <i className={`fa ${expandido ? 'fa-magnifying-glass' : 'fa-magnifying-glass'}`}></i>
+                {expandido && <span>Busca Avançada</span>}
+            </Link>
+         </li>
+         <li>
+             <Link to="/comparativo">
+                <i className={`fa ${expandido ? 'fa-square-poll-vertical' : 'fa-square-poll-vertical'}`}></i>
+                {expandido && <span>Comparativo</span>}
+            </Link>
+         </li>
+           <li onClick={handleLogout} style={{ cursor: 'pointer', color: '#dc3545' }}>
+            <i className={`fa ${expandido ? 'fa-right-from-bracket' : 'fa-sign-out-alt'}`}></i>
+            {expandido && <span>Logout</span>}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <Outlet/>
+    </>
+  );
+};
+
+export default MenuLaboratorio;
